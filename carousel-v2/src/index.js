9afdae2a686821d70ef4f9084c93c36a7912450b
Bernard <bernardy_ho@hotmail.com>
2021-11-29 23:26:55 +0700

    disecting infinite slide logic

--- carousel-v2/src/index.js
+++ carousel-v2/src/index.js
@@ -57,17 +57,19 @@ function Carousel(options) {
       // buttons: function () {
       //   return showButtons();
       // },
-      // autoplay: function () {
-      //   return play();
-      // },
-      // infinite: function () {
-      //   // 'afterbegin': Just inside the element, before its first child.
-      //   return moveItem(count - 1, -element.offsetTop + "px", "afterbegin");
-      // },
-      // initial: function () {
-      //   var initial = 0 || options.initial >= count ? count : options.initial;
-      //   return show(initial);
-      // },
+      autoplay: function () {
+        return play();
+      },
+      infinite: function () {
+        // 'afterbegin': Just inside the element, before its first child.
+        // 'beforeEnd': Just inside the element, after its last child.
+        // when infinite is turn on the lastelementchild will be moved to the first element with and offsetwidth of -600
+        return moveItem(count - 1, -element.offsetTop + "px", "afterbegin");
+      },
+      initial: function () {
+        var initial = 0 || options.initial >= count ? count : options.initial;
+        return show(initial);
+      },
     };
 
     for (let key in actions) {
@@ -79,6 +81,40 @@ function Carousel(options) {
         actions[key]();
       }
     }
+  }
+
+  /**
+   * Helper for moving items - last to be first or first to be the last. Needed
+   * for infinite rotation of the carousel.
+   *
+   * @param {number} i - Position of the list item to move (either first or last).
+   * @param {number} marginLeft - Left margin to position the item off-screen
+   *        at the beginning or no margin at the end.
+   * @param {string} position - Where to insert the item. One of the following -
+   *        'afterBegin' or 'beforeEnd'.
+   */
+  function moveItem(i, marginLeft, position) {
+    /*CASE: 
+      moveItem({
+        index: 0,
+        marginLeft: "",
+        position: "beforeEnd")
+      }
+    */
+
+    // choose the item to move, in this case its the index 0
+    let itemToMove = element.querySelectorAll("." + crslClass + " > ul li")[i];
+
+    //give the margin left to none if its to the end, and -offsetWidth if its to the front
+    itemToMove.style.marginLeft = marginLeft;
+
+    // remove it because its being move to the back/front
+    element.querySelector("." + crslClass + " > ul").removeChild(itemToMove);
+
+    //Element.insertAdjacentHTML("beforeEnd/afterBegin", <li></li>);
+    element
+      .querySelector("." + crslClass + " > ul")
+      .insertAdjacentHTML(position, itemToMove.outerHTML);
   }
 
   /**
@@ -96,14 +132,14 @@ function Carousel(options) {
     dotContainer.addEventListener("click", function (e) {
       // console.log(e); // PointerEvent Object
       if (e.target.tagName === "LI") {
-        // gets the value of data-position attribute
+        // gets the value of data-position attribute ex: 0/1/2/3
         show(e.target.getAttribute("data-position"));
-        // console.log(e.target);
       }
     });
 
     for (let i = 0; i < count; i++) {
       let dotElement = document.createElement("li");
+      // <li data-position="0/1/2/3" >
       dotElement.setAttribute("data-position", i);
       dotContainer.appendChild(dotElement);
     }
@@ -168,6 +204,21 @@ function Carousel(options) {
   }
 
   /**
+   * Move the carousel backward.
+   *
+   * @public
+   */
+  function showPrev() {
+    if (options.infinite) {
+      showPrevInfinite();
+    } else {
+      showPrevLinear();
+    }
+
+    resetInterval();
+  }
+
+  /**
    * Helper function to show the next slide for LINEAR carousel.
    * If on the last slide - stop the play and do nothing else.
    */
@@ -181,7 +232,45 @@ function Carousel(options) {
       element.querySelectorAll("." + crslClass + " > ul li")[currentIndex]
     );
 
-    adjustCurrent(1);
+    adjustCurrentIndexTo(1);
+  }
+
+  /**
+   * Helper function to show the next slide for INFINITE carousel.
+   * Do the sliding, move the second item to the very end.
+   */
+  function showNextInfinite() {
+    animateNext(element.querySelectorAll("." + crslClass + " > ul li")[1]);
+    moveItem(0, "", "beforeEnd"); // i = 0, marginLeft = "", position = before the end
+
+    adjustCurrentIndexTo(1);
+  }
+
+  /**
+   * Helper function to show the previous slide for LINEAR carousel.
+   * Stop the autoplay if user goes back. If on the first slide - do nothing.
+   */
+  function showPrevLinear() {
+    stop();
+    if (currentIndex === 0) {
+      return;
+    }
+    animatePrev(
+      element.querySelectorAll("." + crslClass + " > ul li")[currentIndex - 1]
+    );
+
+    adjustCurrentIndexTo(-1);
+  }
+
+  /**
+   * Helper function to show the previous slide for INFINITE carousel.
+   * Do the sliding, move the last item to the very beginning.
+   */
+  function showPrevInfinite() {
+    animatePrev(element.querySelectorAll("." + crslClass + " > ul li")[0]);
+    moveItem(count - 1, -element.offsetWidth + "px", "afterBegin");
+
+    adjustCurrentIndexTo(-1);
   }
 
   /**
@@ -189,17 +278,21 @@ function Carousel(options) {
    *
    * @param {number} val - defines which way current should be corrected.
    */
-  function adjustCurrent(val) {
-    currentIndex += val;
+  function adjustCurrentIndexTo(val) {
+    currentIndex += val; // currentIndex = currentIndex + val (val default value is 1)
+    console.log(currentIndex); // current index is x (starts from 0) than x=0+1 ,, x=1+1,, x=2+1
 
     switch (currentIndex) {
       case -1:
         currentIndex = count - 1;
+        alert("case is -1");
         break;
-      case count:
+      case count: // atm the count is 5
         currentIndex = 0;
+        alert("case is count");
         break;
       default:
+        // alert("default");
         currentIndex = currentIndex;
     }
 
@@ -215,9 +308,49 @@ function Carousel(options) {
    */
   function animateNext(item) {
     item.style.marginLeft = -element.offsetWidth + "px";
+    // width is 600px so.. -600px
   }
 
-  function showPrev() {
-    console.log("show prev");
+  /**
+   * Animate the carousel to go back 1 slide. Moves the very first (off-screen)
+   * item to the visible area.
+   *
+   * @param {object} item - The element to move into view.
+   */
+  function animatePrev(item) {
+    item.style.marginLeft = "";
+  }
+
+  /**
+   * Reset the autoplay interval.
+   */
+  function resetInterval() {
+    if (cycle) {
+      stop();
+      play();
+    }
+  }
+
+  /**
+   * Start the auto play.
+   * If already playing do nothing.
+   *
+   * @public
+   */
+  function play() {
+    if (cycle) {
+      return;
+    }
+    cycle = setInterval(showNext.bind(this), interval);
+  }
+
+  /**
+   * Stop the auto play.
+   *
+   * @public
+   */
+  function stop() {
+    clearInterval(cycle);
+    cycle = null;
   }
 }
